c3c2ba9cc3157cfaacc7fb98d24e704637b2870f
Remstedt, Alex <49374085+AlexRemstedt@users.noreply.github.com>
2026-03-25 17:36:52 +0100

    rename strain_plotter -> plotters.py

--- src/plotters.py
+++ src/plotters.py
@@ -6,3 +6,9 @@ def plot_strain(df: pd.DataFrame):
     """Create a plot of the strain data using ggplot."""
     p = ggplot(df, aes(x="x", y="strain", color="line")) + geom_line()
     ggsave(p, "strain.png")
+
+
+def plot_stress(df: pd.DataFrame):
+    """Create a plot of the stress data using ggplot."""
+    p = ggplot(df, aes(x="x", y="stress", color="line")) + geom_line()
+    ggsave(p, "stress.png")
